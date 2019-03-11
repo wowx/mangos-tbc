ef5f915f1afd51b362c1c2563ffb75c4078e0ee7
wowx <wowx@zoznam.sk>
2019-03-11 15:17:48 +0100

    fix previous commit

--- src/game/AI/ScriptDevAI/scripts/eastern_kingdoms/sunwell_plateau/boss_kiljaeden.cpp
+++ src/game/AI/ScriptDevAI/scripts/eastern_kingdoms/sunwell_plateau/boss_kiljaeden.cpp
@@ -484,9 +484,14 @@ struct boss_kiljaedenAI : public Scripted_NoMovementAI, private DialogueHelper
         }
         else if (pSummoned->GetEntry() == NPC_SHIELD_ORB)
         {
-            pSummoned->CastSpell(pSummoned, SPELL_SHADOW_BOLT_AURA, TRIGGERED_OLD_TRIGGERED);
-            pSummoned->AI()->AttackStart(pPlayer);
-
+            if (pSummoned->IsTemporarySummon())
+            {
+                if (Player* pPlayer = m_creature->GetMap()->GetPlayer(pSummoned->GetSpawnerGuid()))
+                {
+                    pSummoned->CastSpell(pSummoned, SPELL_SHADOW_BOLT_AURA, TRIGGERED_OLD_TRIGGERED);
+                    pSummoned->AI()->AttackStart(pPlayer);
+                }
+            }
             // Start the movement of the shadow orb - calculate new position based on the angle between the boss and orb
             float fX, fY;
             float fAng = m_creature->GetAngle(pSummoned) + M_PI_F / 8;
